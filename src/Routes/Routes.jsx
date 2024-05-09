d203446209433027f86380a8ce1e26bd083a1523
sojibhasan9878 <sojibhasan9878@gmail.com>
2024-05-09 18:58:33 +0600

    all files and folders created

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div className="text-3xl text-red-600 font-bold">clint site file complete push ready <button className="btn btn-accent">Accent</button></div>,
+    },
+  ]);
