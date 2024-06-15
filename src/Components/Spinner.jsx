f32ae7f885a6b25988beb2b198668b96e831192c
sojibhasan9878 <sojibhasan9878@gmail.com>
2024-06-15 18:51:30 +0600

    all up to date

--- src/Components/Spinner.jsx
+++ src/Components/Spinner.jsx
@@ -1,11 +1,6 @@
-import { Helmet } from "react-helmet";
-
 const Spinner = () => {
   return (
     <div className="flex justify-center items-center min-h-[calc(100vh-92px)]">
-      <Helmet>
-        <title>Loading</title>
-      </Helmet>
       <span className="loading loading-spinner loading-lg text-secondary"></span>
     </div>
   );
